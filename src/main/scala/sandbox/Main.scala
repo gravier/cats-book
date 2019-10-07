6ef1db0fdb42953b442c22238710ef411522230f
evaldas <e.miliauskas@fuzzylabsresearch.com>
2019-10-07 22:32:45 +0300

    invariant imap implement

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -5,6 +5,7 @@ import cats.syntax.functor._
 import functors._
 import functors.CustomFunctor._
 import functors.Printers._
+import functors.Codecs._
 //import functors.Printers.booleanPrintable._
 //import functors.Printers.stringPrintable._
 //import functors.Printers.boxPrintable._
@@ -23,5 +24,12 @@ object Main extends App {
   format(true)
   println("whats in box " + format(Box("hello box")))
   println("whats in box 2 " + format(Box(false)))
-  println("whats in box 2 " + format(Box(1)))
+//  println("whats in box 2 " + format(Box(1)))
+
+  println(
+    s"encoding to string works - ${encode(123.4)} ${encode(123.4) == "123.4"}")
+  // res0: String = 123.4
+  println(
+    s"decoding to double works - ${decode[Double]("123.4")} ${decode[Double]("123.4") == 123.4}")
+  // res1: Double = 123.4
 }
---
+++ src/main/scala/sandbox/functors/InvariantPrintable.scala
@@ -0,0 +1,24 @@
+package sandbox.functors
+
+trait Codec[A] {
+  self =>
+  def encode(value: A): String
+  def decode(value: String): A
+  def imap[B](dec: A => B, enc: B => A): Codec[B] = new Codec[B] {
+    def encode(value: B): String = self.encode(enc(value))
+    def decode(value: String): B = dec(self.decode(value))
+  }
+}
+
+object Codecs {
+  def encode[A](value: A)(implicit c: Codec[A]): String =
+    c.encode(value)
+  def decode[A](value: String)(implicit c: Codec[A]): A =
+    c.decode(value)
+
+  implicit val doubleCodec: Codec[Double] =
+    new Codec[Double] {
+      def encode(value: Double): String = value.toString
+      def decode(value: String): Double = value.toDouble
+    }
+}
